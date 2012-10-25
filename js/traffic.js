da642f57f9aa309f4a54b153d46fb0df61c7eaee
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-10-25 13:25:01 +0200

    default configuration that enables traffic jams

--- js/traffic.js
+++ js/traffic.js
@@ -1,13 +1,13 @@
-(function($, Traffic){
+(function($, _, Traffic){
     var driver = new Traffic.Driver.ruleBased();
     driver.add(new Traffic.Driver.Rule({ "suggestion" : 0 }));
 
     var road = new Traffic.Road({ "driver" : driver });
-    road.at(1).place(new Traffic.Car({ "speed" : 0 }));
-    road.at(2).place(new Traffic.Car({ "speed" : 0 }));
-    road.at(3).place(new Traffic.Car({ "speed" : 0 }));
+    _.each(_.range(6), function(index){
+	road.at(index + 1).place(new Traffic.Car({ "speed" : 0 }));
+    }, road);
     
     $(function(){
 	new Traffic.MainView({ model : road, el : $("body") });
     });
-})(jQuery, Traffic);
+})(jQuery, _, Traffic);
